6112e6d4a77e7e0950bca61b439d3a8b4e139462
JohnR46 <Johnnote7@gmail.com>
2021-12-09 01:27:27 +0700

    EP7

--- main.py
+++ main.py
@@ -11,12 +11,9 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print_hi('EP7 ')
 
-    main_in_hat_en =  'A' + ' the man in the hat. '
+    main_in_hat_en = 'A' + ' the man in the hat. '
     print(main_in_hat_en.strip())
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
-
-
-
